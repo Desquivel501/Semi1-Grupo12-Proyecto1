edb29899ea31cea61093c1e916de51055f3d48f0
DanielAcabal <danchiacabal@gmail.com>
2023-09-05 17:45:49 -0600

    Add artist controller

--- Backend/Nodejs/src/models/artist.ts
+++ Backend/Nodejs/src/models/artist.ts
@@ -1,12 +1,44 @@
+import { pool } from "./database/connection";
 import { Artist } from "./types";
 
 export class ArtistModel {
-  static createArtist(artist: Artist) {
+  static async createArtist(artist: Artist, photo: Express.MulterS3.File) {
+    const newAvatar: Artist = { ...artist };
+    newAvatar.avatar = photo.location;
+    // Guardar en DB
+    console.log(newAvatar);
+    return true;
   }
   static getArtist({ id }: { id: number }) {
+    try {
+      const res = pool.query(
+        `SELECT * FROM Artist WHERE id_artist=${id}`,
+        (error, result, fields) => {
+          if (error) throw error;
+          return result;
+        },
+      );
+      return { res, ok: true };
+    } catch (error) {
+      console.log(error);
+      return { error, ok: false };
+    }
   }
   static editArtist({ data }: { data: any }) {
   }
   static deleteArtist({ id }: { id: number }) {
+    try {
+      const res = pool.query(
+        `DELETE FROM Artist WHERE id_artist=${id}`,
+        (error, result, fields) => {
+          if (error) throw error;
+          return result;
+        },
+      );
+      return { res, ok: true };
+    } catch (error) {
+      console.log(error);
+      return { error, ok: false };
+    }
   }
 }
