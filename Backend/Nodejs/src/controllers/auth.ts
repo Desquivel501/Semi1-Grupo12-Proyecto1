092a746d92b2a0f951bb0b5d74ee80b98fbe92d6
DanielAcabal <danchiacabal@gmail.com>
2023-09-06 12:19:05 -0600

    Add login

--- Backend/Nodejs/src/controllers/auth.ts
+++ Backend/Nodejs/src/controllers/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Credentials } from "../models/types";
+import { AuthModel } from "../models/auth";
 
 export class AuthController {
   static login(req: Request, res: Response) {
@@ -7,7 +8,11 @@ export class AuthController {
     if (!email || !password) {
       return res.status(401).json({ message: "Faltan datos" });
     }
-    res.json({ message: "Bienvenido" });
+    // Login
+    AuthModel.login({ email, password }, (response: string, ok: boolean) => {
+      // Respuesta
+      res.status(ok ? 200 : 400).json(response);
+    });
   }
   static logout(req: Request, res: Response) {
     const { id } = req.params;
--- Backend/Nodejs/src/models/auth.ts
+++ Backend/Nodejs/src/models/auth.ts
@@ -2,16 +2,21 @@ import { pool } from "./database/connection";
 import { Credentials } from "./types";
 
 export class AuthModel {
-  static async login(credentials: Credentials) {
+  static login(credentials: Credentials, callback: Function) {
     try {
-      const response = pool.query("", (err, result) => {
-        if (err) throw err;
-        return result;
-      });
-      return { ok: true, response };
+      pool.query(
+        "CALL Login(?,?)",
+        [credentials.email, credentials.password],
+        (err, result) => {
+          if (err) throw err;
+          if (result[0][0].TYPE == "ERROR") callback(result[0][0], false);
+          else {
+            callback(result[0][0], true);
+          }
+        },
+      );
     } catch (error) {
-      console.log(error);
-      return { ok: false, error };
+      callback(error, false);
     }
   }
   static logout({ id }: { id: number }) {
